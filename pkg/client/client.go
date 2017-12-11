d335f85d58568a2f5be7ad98087b26515bc9e855
zhangxu19830126@gmail.com <zhangxu19830126@gmail.com>
2017-12-11 11:14:32 +0800

    dev: add cluster and server rpc client  impl

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"fmt"
+	"io"
 
 	"github.com/fagongzi/gateway/pkg/pb/metapb"
 	"github.com/fagongzi/gateway/pkg/pb/rpcpb"
@@ -12,6 +13,14 @@ import (
 // Client gateway client
 type Client interface {
 	PutCluster(cluster metapb.Cluster) (uint64, error)
+	RemoveCluster(id uint64) error
+	GetCluster(id uint64) (*metapb.Cluster, error)
+	GetClusterList(fn func(*metapb.Cluster) bool) error
+
+	PutServer(server metapb.Server) (uint64, error)
+	RemoveServer(id uint64) error
+	GetServer(id uint64) (*metapb.Server, error)
+	GetServerList(fn func(*metapb.Server) bool) error
 }
 
 // NewClient returns a gateway proxy
@@ -50,6 +59,111 @@ func (c *client) PutCluster(cluster metapb.Cluster) (uint64, error) {
 	}
 
 	return rsp.ID, nil
+}
+
+func (c *client) RemoveCluster(id uint64) error {
+	_, err := c.metaC.RemoveCluster(context.Background(), &rpcpb.RemoveClusterReq{
+		ID: id,
+	}, grpc.FailFast(true))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *client) GetCluster(id uint64) (*metapb.Cluster, error) {
+	rsp, err := c.metaC.GetCluster(context.Background(), &rpcpb.GetClusterReq{
+		ID: id,
+	}, grpc.FailFast(true))
+	if err != nil {
+		return nil, err
+	}
+
+	return rsp.Cluster, nil
+}
+
+func (c *client) GetClusterList(fn func(*metapb.Cluster) bool) error {
+	stream, err := c.metaC.GetClusterList(context.Background(), &rpcpb.GetClusterListReq{}, grpc.FailFast(true))
+	if err != nil {
+		return err
+	}
+
+	for {
+		c, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		next := fn(c)
+		if !next {
+			err = stream.(grpc.ClientStream).CloseSend()
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func (c *client) PutServer(server metapb.Server) (uint64, error) {
+	rsp, err := c.metaC.PutServer(context.Background(), &rpcpb.PutServerReq{
+		Server: server,
+	}, grpc.FailFast(true))
+	if err != nil {
+		return 0, err
+	}
+
+	return rsp.ID, nil
+}
+
+func (c *client) RemoveServer(id uint64) error {
+	_, err := c.metaC.RemoveServer(context.Background(), &rpcpb.RemoveServerReq{
+		ID: id,
+	}, grpc.FailFast(true))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *client) GetServer(id uint64) (*metapb.Server, error) {
+	rsp, err := c.metaC.GetServer(context.Background(), &rpcpb.GetServerReq{
+		ID: id,
+	}, grpc.FailFast(true))
+	if err != nil {
+		return nil, err
+	}
+
+	return rsp.Server, nil
+}
+
+func (c *client) GetServerList(fn func(*metapb.Server) bool) error {
+	stream, err := c.metaC.GetServerList(context.Background(), &rpcpb.GetServerListReq{}, grpc.FailFast(true))
+	if err != nil {
+		return err
+	}
+
+	for {
+		c, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		next := fn(c)
+		if !next {
+			err = stream.(grpc.ClientStream).CloseSend()
+			if err != nil {
+				return err
+			}
+		}
+	}
 }
 
 func createConn(target string, opts *options) (*grpc.ClientConn, error) {
