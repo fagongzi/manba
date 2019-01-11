afd587b8347bdc918af20954dd4f53e71a58ac27
zhangxu <zhangxu19830126@gmail.com>
2019-01-11 21:52:22 +0800

    dev: add lexer and parseer for url

--- pkg/route/route.go
+++ pkg/route/route.go
@@ -1,30 +1,50 @@
 package route
 
+import (
+	"github.com/fagongzi/gateway/pkg/pb/metapb"
+)
+
 type nodeType int
 
 const (
-	numberType = nodeType(1)
-	stringType = nodeType(2)
-	expType    = nodeType(3)
+	slashType  = nodeType(1)
+	constType  = nodeType(2)
+	numberType = nodeType(3)
+	stringType = nodeType(4)
+	enumType   = nodeType(5)
 )
 
 // Route route for api match
-// url define: /path/:namedArg[type]
+// url define: /conststring/(number|string|enum:m1|m2|m3)[:argname]
 type Route struct {
 	root *node
 }
 
 type node struct {
-	value    []byte
 	nt       nodeType
+	value    []byte
+	enums    [][]byte
+	argName  []byte
 	children []node
 }
 
-// Add add a url to this route
-func (r *Route) Add(url []byte) error {
-	n := len(url)
-	for i := 0; i < n; i++ {
+func (n *node) isEnum() bool {
+	return n.nt == enumType
+}
 
-	}
+func (n *node) isConst() bool {
+	return n.nt == constType
+}
+
+func (n *node) addEnum(value []byte) {
+	n.enums = append(n.enums, value)
+}
+
+func (n *node) setArgName(value []byte) {
+	n.argName = value
+}
+
+// Add add a url to this route
+func (r *Route) Add(api metapb.API) error {
 	return nil
 }
--- pkg/route/scanner.go
+++ pkg/route/scanner.go
@@ -1,1 +1,86 @@
 package route
+
+type scanner struct {
+	len   int
+	input []byte
+
+	token int
+	bp    int
+	sp    int
+	ch    byte
+}
+
+func newScanner(input []byte) lexer {
+	scan := &scanner{
+		len:   len(input),
+		input: input,
+		bp:    -1,
+		sp:    0,
+	}
+
+	scan.Next()
+	return scan
+}
+
+func (scan *scanner) Next() byte {
+	scan.bp++
+
+	if scan.bp < scan.len {
+		scan.ch = scan.input[scan.bp]
+	} else {
+		scan.ch = eoi
+	}
+
+	return scan.ch
+}
+
+func (scan *scanner) NextToken() {
+	for {
+		switch scan.ch {
+		case '/':
+			scan.token = tokenSlash
+			scan.Next()
+			return
+		case '(':
+			scan.token = tokenLParen
+			scan.Next()
+			return
+		case '|':
+			scan.token = tokenVertical
+			scan.Next()
+			return
+		case ':':
+			scan.token = tokenColon
+			scan.Next()
+			return
+		case ')':
+			scan.token = tokenRParen
+			scan.Next()
+			return
+		case eoi:
+			scan.token = tokenEOF
+			scan.Next()
+			return
+		}
+
+		scan.Next()
+	}
+}
+
+func (scan *scanner) Current() byte {
+	return scan.ch
+}
+
+func (scan *scanner) Token() int {
+	return scan.token
+}
+
+func (scan *scanner) TokenIndex() int {
+	return scan.bp - 1
+}
+
+func (scan *scanner) ScanString() []byte {
+	value := scan.input[scan.sp : scan.bp-1]
+	scan.sp = scan.bp
+	return value
+}
