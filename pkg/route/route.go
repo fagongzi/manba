e9d47643641bd43a232470c92ac7fa408041bb82
zhangxu <zhangxu19830126@gmail.com>
2019-02-01 16:26:47 +0800

    Revert "dev:  update route"
    This reverts commit 5bd16449d888c50b1842fb98a711c07944351a6c.

--- pkg/route/route.go
+++ pkg/route/route.go
@@ -2,32 +2,49 @@ package route
 
 import (
 	"github.com/fagongzi/gateway/pkg/pb/metapb"
-	"github.com/fagongzi/util/hack"
+)
+
+type nodeType int
+
+const (
+	slashType  = nodeType(1)
+	constType  = nodeType(2)
+	numberType = nodeType(3)
+	stringType = nodeType(4)
+	enumType   = nodeType(5)
 )
 
 // Route route for api match
 // url define: /conststring/(number|string|enum:m1|m2|m3)[:argname]
 type Route struct {
-	root node
+	root *node
 }
 
-// NewRoute create a api match route
-func NewRoute() *Route {
-	return &Route{
-		root: node{
-			nt:    slashType,
-			value: slashValue,
-		},
-	}
+type node struct {
+	nt       nodeType
+	value    []byte
+	enums    [][]byte
+	argName  []byte
+	children []node
+}
+
+func (n *node) isEnum() bool {
+	return n.nt == enumType
+}
+
+func (n *node) isConst() bool {
+	return n.nt == constType
+}
+
+func (n *node) addEnum(value []byte) {
+	n.enums = append(n.enums, value)
+}
+
+func (n *node) setArgName(value []byte) {
+	n.argName = value
 }
 
 // Add add a url to this route
 func (r *Route) Add(api metapb.API) error {
-	p := newParser(hack.StringToSlice(api.URLPattern))
-	_, err := p.parse()
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
