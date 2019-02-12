fb78d20fa74c24cbf58076a0cffc050c680a73e3
zhangxu <zhangxu19830126@gmail.com>
2019-02-12 14:45:36 +0800

    dev: add find route

--- pkg/route/route_test.go
+++ pkg/route/route_test.go
@@ -86,3 +86,61 @@ func TestAdd(t *testing.T) {
 		t.Errorf("expect api 4 children but %d", r.root.children[4].api)
 	}
 }
+
+func TestFind(t *testing.T) {
+	r := NewRoute()
+	r.Add(metapb.API{
+		ID:         1,
+		URLPattern: "/",
+	})
+	r.Add(metapb.API{
+		ID:         2,
+		URLPattern: "/check",
+	})
+	r.Add(metapb.API{
+		ID:         3,
+		URLPattern: "/(string)",
+	})
+	r.Add(metapb.API{
+		ID:         4,
+		URLPattern: "/(number)",
+	})
+	r.Add(metapb.API{
+		ID:         5,
+		URLPattern: "/(enum:on|off)",
+	})
+
+	id, _ := r.Find([]byte("/"))
+	if id != 1 {
+		t.Errorf("expect matched 1, but %d", id)
+	}
+
+	id, _ = r.Find([]byte("/check"))
+	if id != 2 {
+		t.Errorf("expect matched 2, but %d", id)
+	}
+
+	id, _ = r.Find([]byte("/check2"))
+	if id != 3 {
+		t.Errorf("expect matched 3, but %d", id)
+	}
+
+	id, _ = r.Find([]byte("/123"))
+	if id != 4 {
+		t.Errorf("expect matched 4, but %d", id)
+	}
+
+	id, _ = r.Find([]byte("/on"))
+	if id != 5 {
+		t.Errorf("expect matched 5, but %d", id)
+	}
+	id, _ = r.Find([]byte("/off"))
+	if id != 5 {
+		t.Errorf("expect matched 5, but %d", id)
+	}
+
+	id, _ = r.Find([]byte("/on/notmatches"))
+	if id != 0 {
+		t.Errorf("expect not matched , but %d", id)
+	}
+}
