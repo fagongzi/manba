f0edda004ea5cb470e759a52a869a055de83d205
zhangxu <zhangxu19830126@gmail.com>
2019-02-19 11:37:42 +0800

    dev: js module

--- pkg/plugin/builtin_log.go
+++ pkg/plugin/builtin_log.go
@@ -4,66 +4,66 @@ import (
 	"github.com/fagongzi/log"
 )
 
-// Log builtin log
-type Log struct {
+// LogModule log module
+type LogModule struct {
 }
 
 // Info info
-func (l *Log) Info(v ...interface{}) {
+func (l *LogModule) Info(v ...interface{}) {
 	log.Info(v...)
 }
 
 // Infof infof
-func (l *Log) Infof(format string, v ...interface{}) {
+func (l *LogModule) Infof(format string, v ...interface{}) {
 	log.Infof(format, v...)
 }
 
 // Debug debug
-func (l *Log) Debug(v ...interface{}) {
+func (l *LogModule) Debug(v ...interface{}) {
 	log.Debug(v...)
 }
 
 // Debugf debugf
-func (l *Log) Debugf(format string, v ...interface{}) {
+func (l *LogModule) Debugf(format string, v ...interface{}) {
 	log.Debugf(format, v...)
 }
 
 // Warn warn
-func (l *Log) Warn(v ...interface{}) {
+func (l *LogModule) Warn(v ...interface{}) {
 	log.Warning(v...)
 }
 
 // Warnf warnf
-func (l *Log) Warnf(format string, v ...interface{}) {
+func (l *LogModule) Warnf(format string, v ...interface{}) {
 	log.Warningf(format, v...)
 }
 
 // Warning warning
-func (l *Log) Warning(v ...interface{}) {
+func (l *LogModule) Warning(v ...interface{}) {
 	log.Warning(v...)
 }
 
 // Warningf warningf
-func (l *Log) Warningf(format string, v ...interface{}) {
+func (l *LogModule) Warningf(format string, v ...interface{}) {
 	log.Warningf(format, v...)
 }
 
 // Error error
-func (l *Log) Error(v ...interface{}) {
+func (l *LogModule) Error(v ...interface{}) {
 	log.Error(v...)
 }
 
 // Errorf errorf
-func (l *Log) Errorf(format string, v ...interface{}) {
+func (l *LogModule) Errorf(format string, v ...interface{}) {
 	log.Errorf(format, v...)
 }
 
 // Fatal fatal
-func (l *Log) Fatal(v ...interface{}) {
+func (l *LogModule) Fatal(v ...interface{}) {
 	log.Fatal(v...)
 }
 
 // Fatalf fatalf
-func (l *Log) Fatalf(format string, v ...interface{}) {
+func (l *LogModule) Fatalf(format string, v ...interface{}) {
 	log.Fatalf(format, v...)
 }
