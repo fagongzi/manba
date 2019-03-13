3d7bc93ccbd94a088f4940022d67f0239d3ddd43
weihl <weihailang@gmail.com>
2019-03-13 15:30:04 +0800

    dockerfile all in one, support none root user bind port 80

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,50 @@
-#!/bin/bash
+#!/bin/sh
+
+set -e
 
 start_etcd() {
-    ./etcd &
+    ./etcd $ETCD_OPTS &
 }
 
 start_apiserver() {
-    ./apiserver --addr=:9092 --addr-http=:9093 --discovery &
+    ./apiserver --addr=:9092 --addr-http=:9093 --discovery $API_SERVER_OPTS &
 }
 
-start_proxy() {
-    ./proxy --addr=:80 --log-level=$GATEWAY_LOG_LEVEL
-}
+PARAM=$1
+CMD=`cat cmd`
+if [ "$PARAM" = "" ]
+then
+    PARAM=${CMD}
+fi
 
-start_etcd
-sleep 3
-start_apiserver
-sleep 1
-start_proxy
+DEFAULT_EXEC="./proxy --addr=:80 --log-level=$GATEWAY_LOG_LEVEL $GW_PROXY_OPTS"
+if [ "${PARAM}" = 'demo' ]
+then
+    start_etcd
+    sleep 3
+    start_apiserver
+    sleep 1
+    EXEC=$DEFAULT_EXEC
+fi
 
+if [ "${PARAM}" = 'proxy' ]
+then
+    EXEC=$DEFAULT_EXEC
+fi
+
+if [ "${PARAM}" = 'apiserver' ]
+then
+    EXEC="./apiserver --addr=:9092 --addr-http=:9093 --discovery $API_SERVER_OPTS"
+fi
+
+if [ "${PARAM}" = 'etcd' ]
+then
+    EXEC="./etcd $ETCD_OPTS"
+fi
+
+if [ ! -z "${PARAM}" ] && [ -z "$EXEC" ]
+then
+    EXEC="${PARAM}"
+fi
+
+exec $EXEC
