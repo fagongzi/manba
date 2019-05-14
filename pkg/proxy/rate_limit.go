5aa434a81860de635d4a195c62dea85a1e993166
zhangxu <zhangxu19830126@gmail.com>
2019-05-14 14:43:23 +0800

    add rate limit option

---
+++ pkg/proxy/rate_limit.go
@@ -0,0 +1,29 @@
+package proxy
+
+import (
+	"time"
+
+	"github.com/fagongzi/gateway/pkg/pb/metapb"
+	"github.com/juju/ratelimit"
+)
+
+type rateLimiter struct {
+	limiter *ratelimit.Bucket
+	option  metapb.RateLimitOption
+}
+
+func newRateLimiter(max int64, option metapb.RateLimitOption) *rateLimiter {
+	return &rateLimiter{
+		limiter: ratelimit.NewBucket(time.Second/time.Duration(max), max),
+		option:  option,
+	}
+}
+
+func (l *rateLimiter) do(count int64) bool {
+	if l.option == metapb.Wait {
+		l.limiter.Wait(count)
+		return true
+	}
+
+	return l.limiter.TakeAvailable(count) > 0
+}
