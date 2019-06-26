630947c0116ff5e79ecb8a3efa5231e25d4202ec
zhangxu <zhangxu19830126@gmail.com>
2019-06-26 10:08:08 +0800

    fix compile

--- pkg/proxy/dispatcher_meta.go
+++ pkg/proxy/dispatcher_meta.go
@@ -625,7 +625,7 @@ func (r *dispatcher) maybeUpdateJSEngine(id uint64) error {
 
 func (r *dispatcher) updateJSEngine() error {
 	var plugins []*metapb.Plugin
-	newEngine := plugin.NewEngine()
+	newEngine := plugin.NewEngine(r.cnf.Option.EnableJSPlugin)
 	for _, id := range r.appliedPlugins.AppliedIDs {
 		p := r.plugins[id]
 		plugins = append(plugins, p)
