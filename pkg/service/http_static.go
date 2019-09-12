e53c5ed85a06dba789126f408c1eeff29d789ec2
zhangxu <zhangxu19830126@gmail.com>
2019-09-12 14:29:49 +0800

    fix ui

--- pkg/service/http_static.go
+++ pkg/service/http_static.go
@@ -1,9 +1,12 @@
 package service
 
 import (
+	"fmt"
+
 	"github.com/labstack/echo"
 )
 
 func initStatic(server *echo.Echo, ui, uiPrefix string) {
 	server.Static(uiPrefix, ui)
+	server.Static("static", fmt.Sprintf("%s/static", ui))
 }
